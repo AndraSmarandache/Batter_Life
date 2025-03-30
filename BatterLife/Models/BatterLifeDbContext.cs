08a87a352bacb7efcdc3735620fa7cb4cb81db38
Andra <smarandache.andra.e3s@student.ucv.ro>
2025-03-30 16:35:01 +0300

    Initial db connection

---
+++ BatterLife/Models/BatterLifeDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BatterLife.Models
+{
+    public class BatterLifeDbContext : DbContext
+    {
+        public BatterLifeDbContext(DbContextOptions<BatterLifeDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Product> Products { get; set; } // Adaugă și celelalte modele dacă sunt necesare
+    }
+}
--- BatterLife/Program.cs
+++ BatterLife/Program.cs
@@ -1,8 +1,12 @@
+using BatterLife.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<BatterLifeDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BatterLifeDb")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
