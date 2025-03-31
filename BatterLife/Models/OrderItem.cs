bd850b222880688c332485e6f0f935d080be5efd
Andra <smarandache.andra.e3s@student.ucv.ro>
2025-03-31 03:42:30 +0300

    Some model attribute changes

--- BatterLife/Models/OrderItem.cs
+++ BatterLife/Models/OrderItem.cs
@@ -8,6 +8,5 @@
         public int ProductId { get; set; }
         public Product Product { get; set; }
         public int Quantity { get; set; }
-        public decimal UnitPrice { get; set; }
     }
 }
